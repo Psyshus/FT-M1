8e3337c377529ff78553d9314d443979a2f814b7
Psyshus <erick.vasquez.lopez00@gmail.com>
2022-04-11 17:29:30 -0500

    Home work resuelto binario a decimal y viceversa

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,17 +1,29 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let numDecimal = 0;
+  const longitudDeNum = num.length;
 
+  for (let i = 0; i < longitudDeNum; i++) {
+    let posicion = longitudDeNum - 1 - i;
+    let basePotencia = 2 ** i;
+    numDecimal += num[posicion] * basePotencia;
+  }
+  return numDecimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let arrayBin = [];
+  while (num > 0) {
+    arrayBin.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return arrayBin.reverse().join("");
 }
-
 
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
-}
+};
